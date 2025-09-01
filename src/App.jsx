bee90323c3fadd85ae3db5e7aa45e645c6f35977
Suzet Salinas <suzet.sal@gmail.com>
2025-08-31 20:07:40 -0500

    updated styling; got it to show correctly in browser

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Layout from './components/layout'
 import Sidebar from './components/sidebar'
 import Content from './components/content'
-import General from './pages/Settings/general'
+import General from './pages/settings/general'
 import Basic from './pages/settings/basic'
+import './styles/App.css'
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* nested routes within Layout component wrapper */}
-        <Route>path="/" element={<Layout />}
-            <Route>index element={<General />}</Route>
-            <Route>path="/basic" element={<Basic />}</Route>
+        <Route path="/" element={<Layout />}>
+            <Route index element={<General />}></Route>
+            <Route path="basic" element={<Basic />}></Route>
             {/* todo: add routes for rest of pages */}
         </Route>
         
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,1 +1,10 @@
 //sidebar menu
+import React from "react";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">Settings</div>
+    )
+}
+
+export default Sidebar
